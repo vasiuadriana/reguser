3e23379fa4e2a87a67e16ea4a868aa41ebea0a53
Jordan Dimov <jdimov@a115.co.uk>
2014-06-06 00:45:05 +0100

    cleanup

--- reguser/forms.py
+++ reguser/forms.py
@@ -1,6 +1,5 @@
 import re
 from django import forms
-#from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 from reguser.utils import generate_username
