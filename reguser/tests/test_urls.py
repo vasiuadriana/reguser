60e3f4af3f931562ad7fa10ed05d9a829d0927d6
Jordan Dimov <jdimov@a115.co.uk>
2014-06-06 20:02:09 +0100

    URL test improvement

--- reguser/tests/test_urls.py
+++ reguser/tests/test_urls.py
@@ -1,7 +1,12 @@
 from lunchbag.testutils import URLTestCase
+from django.core.urlresolvers import reverse
 from reguser import views
 
 class ReguserURLsTestCase(URLTestCase):
+    def setUp(self):
+        reference_url = reverse('reguser-registration')
+        self.assertTrue(reference_url.endswith('register/'))
+        self.url_base = reference_url[:-len('register/')]
 
     def test_registration_url(self):
-        self.assert_url_matches_view(views.registration, '/accounts/register/', 'reguser-registration')
+        self.assert_url_matches_view(views.registration, self.url_base+'register/', 'reguser-registration')
