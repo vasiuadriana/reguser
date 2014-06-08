97f2b51292e6211e77900da956d532b917c1ec49
Jordan Dimov <jdimov@a115.co.uk>
2014-06-08 19:16:04 +0100

    Management command fix

--- reguser/management/commands/create_pg_email_index.py
+++ reguser/management/commands/create_pg_email_index.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
-from django.db.utils import OperationalError
-
+from django.db.utils import OperationalError, ProgrammingError
 class Command(BaseCommand):
     help = "Creates an unique index on the email field of the user table (PostgreSQL)"
 
@@ -11,4 +10,6 @@ class Command(BaseCommand):
             cursor.execute("CREATE UNIQUE INDEX auth_user_unique_email ON auth_user (email)")
             print "Created unique index on email field."
         except OperationalError, e:
+            print e
+        except ProgrammingError, e:
             print e
